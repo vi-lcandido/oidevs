c1dccaf156ec8b7bf80bb388b75b460734d505dc
vi-lcandido <vitoria.lcandido@gmail.com>
2023-01-10 18:05:49 -0300

    feat - creating react app with JS

--- curso-formacao/poo/aula2/exercicios.js/letramaisucula.js
+++ curso-formacao/poo/aula2/exercicios.js/letramaisucula.js
@@ -1,18 +1,16 @@
 class StringMelhor extends String {
-    palavra;
-    constructor(palavra) {
-      super()
-      this.palavra = palavra;
-    }
-  
-    primeiraLetraMaiuscula() {
-      return this.palavra.charAt(0).toUpperCase() + this.palavra.slice(1).toLowerCase();
-    }
+  palavra;
+  constructor(palavra) {
+    super(palavra);
   }
-  
-  const str = new StringMelhor("exemplo");
-  console.log(str.primeiraLetraMaiuscula());
-  
-  const outraStr = new StringMelhor("exemPLO");
-  console.log(outraStr.primeiraLetraMaiuscula());
-  
+
+  primeiraLetraMaiuscula() {
+    return this.charAt(0).toUpperCase() + this.slice(1).toLowerCase();
+  }
+}
+
+const str = new StringMelhor("exemplo");
+console.log(str.primeiraLetraMaiuscula());
+
+const outraStr = new StringMelhor("exemPLO");
+console.log(outraStr.primeiraLetraMaiuscula());
