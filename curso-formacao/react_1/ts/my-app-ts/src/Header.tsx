a9a85fe60622a7a4880dcbd7eba808e78f4455b4
vi-lcandido <vitoria.lcandido@gmail.com>
2023-01-10 20:53:04 -0300

    feat - react ts products and home

---
+++ curso-formacao/react_1/ts/my-app-ts/src/Header.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <button>
+        <a href="/">Home</a>
+      </button>
+
+      <button>
+        <a href="/produtos">Produtos</a>
+      </button>
+    </header>
+  );
+};
+
+export default Header;
---
+++ curso-formacao/react_1/ts/my-app-ts/src/Titulo.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Titulo = ({ texto }:{texto:string}) => {
+  return <h1 style={{ color: "#018" }}>{texto}</h1>;
+};
+
+export default Titulo;
